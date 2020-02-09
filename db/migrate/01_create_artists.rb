ae1e87a0af6567196d744b395861f940b1e433b6
Oliver Dahl <oliverpdahl@gmail.com>
2020-02-09 13:14:57 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,9 +1,8 @@
 # db/migrate/01_create_artists.rb
 
-class CreateArtists < ActiveRecord::Migration[5.2]
-  def up
-  end
+# db/migrate/01_create_artists.rb
 
-  def down
+class CreateArtists < ActiveRecord::Migration[5.2]
+  def change
   end
 end
